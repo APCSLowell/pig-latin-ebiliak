da096815ef1e2c91e5fa83b05a6feeb8de6b0a31
ebiliak <ilforoughi@s.sfusd.edu>
2025-01-24 21:37:25 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -1,6 +1,6 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.File; 
+import java.io.FileNotFoundException;  
+import java.util.Scanner; 
 
 public class PigLatin {
 
@@ -28,39 +28,37 @@ public class PigLatin {
     }
 
     public int findFirstVowel(String sWord) {
-        // Precondition: sWord is a valid String of length greater than 0.
-        // Postcondition: Returns the position of the first vowel in sWord. If there are no vowels, returns -1
+   
         for (int i = 0; i < sWord.length(); i++) {
             char c = sWord.charAt(i);
             if ("aeiou".indexOf(c) != -1) {
-                return i; // Return the index of the first vowel
+                return i; 
             }
         }
         return -1; // No vowels found
     }
 
     public String pigLatin(String sWord) {
-        // Precondition: sWord is a valid String of length greater than 0
-        // Postcondition: Returns the Pig Latin equivalent of sWord
+  
 
         int firstVowelPos = findFirstVowel(sWord);
 
-        // Rule 1: Words with no vowels
+
         if (firstVowelPos == -1) {
             return sWord + "ay";
         }
 
-        // Rule 2: Words that start with a vowel
+
         if (firstVowelPos == 0) {
             return sWord + "way";
         }
 
-        // Rule 3: Words that start with "qu"
+ 
         if (sWord.startsWith("qu")) {
             return sWord.substring(2) + "quay";
         }
 
-        // Rule 4: Words that start with consonants
+  
         return sWord.substring(firstVowelPos) + sWord.substring(0, firstVowelPos) + "ay";
     }
 
